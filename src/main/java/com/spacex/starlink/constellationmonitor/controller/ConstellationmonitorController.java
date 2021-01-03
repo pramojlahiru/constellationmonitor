70f42709a23f97eb552dea87fc39bc2a66928c96
Pramoj <pramoj.kumararathna@ge.com>
2021-01-04 03:40:49 +0530

    end point type change

--- src/main/java/com/spacex/starlink/constellationmonitor/controller/ConstellationmonitorController.java
+++ src/main/java/com/spacex/starlink/constellationmonitor/controller/ConstellationmonitorController.java
@@ -10,6 +10,7 @@ package com.spacex.starlink.constellationmonitor.controller;
 import com.spacex.starlink.constellationmonitor.model.ConstellationAssign;
 import com.spacex.starlink.constellationmonitor.service.ConstellationMonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class ConstellationmonitorController {
     @Autowired
     ConstellationMonitorService constellationMonitorService;
 
-    @PostMapping(value = "/fetch")
+    @GetMapping(value = "/fetch")
     public List<ConstellationAssign> fetchConstellations() {
         return constellationMonitorService.fetchConstellations();
     }
